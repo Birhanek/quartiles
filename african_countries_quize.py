1f6f53f471f9b5f2ed28596b5a8ad9f35665c703
Birhane Kahsay <birhane.kahsay99@gmail.com>
2024-04-22 23:03:23 +0200

    Encryption and Decryption of message

--- african_countries_quize.py
+++ african_countries_quize.py
@@ -23,11 +23,77 @@ def guess():
                 lives -=1
         else:
             break
-
-
            
-    print(f'You guess only {score} countries')        
-guess()
+    print(f' Game Over! You guess only {score} countries')        
+#guess()
+
+
+class caesar:
+
+    """
+    In cryptography, a Caesar cipher, also known as shift cipher, is one of the simplest and most widely known encryption techniques.
+    It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet.
+    For example, with a left shift of 3, D would be replaced by A, E would become B, and so on.
+    The method is named after Julius Caesar, who used it in his private correspondence.
+    
+    """
+
+    def __init__(self,key:int) -> None:
+        self.key = key
+    # To  encrypt a given message based on a specified key
+    def encrypt(self,plain_text:str):
+        alphabets ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        alphabets = alphabets.lower()
+        list_alphabets = list(alphabets)
+
+        cipher_text =''
+        sequence = 0
+
+        plain_text = plain_text.replace(' ','').lower()
+        list_plain_text = list(plain_text)
+        print(plain_text)
+        for letter in list_plain_text:  
+            if letter in list_alphabets:
+                index = alphabets.index(letter)
+                sequence = (index + self.key) % 26
+                cipher_text += alphabets[sequence]
+        return cipher_text
+    
+    # To decrypt a give secret message in to a human understandable language
+    def decrypt(self,cipher_text:str):
+        alphabets ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        alphabets = alphabets.lower()
+        list_alphabets = list(alphabets)
+        list_cipher = list(cipher_text)
+
+        plain_text =''
+        sequence = 0
+        for letter in list_cipher:
+            if letter in list_alphabets:
+                index = alphabets.index(letter)
+                sequence = (index - self.key) % 26
+                plain_text += alphabets[sequence]
+        return plain_text
+
+
+
+caesar_cipher = caesar(5)
+
+encrypted = caesar_cipher.encrypt("Use a Python program to decode the following secret message")
+print(encrypted)
+
+print()
+
+plain_text = caesar_cipher.decrypt(encrypted)
+print(plain_text)
+
+print()
+
+print(caesar_cipher.decrypt('N qtaj uwtlwfrrnsl zxnsl Udymts'))
+
+        
+
+
 
 
 
