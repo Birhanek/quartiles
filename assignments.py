7d0bb49f849d6e63be39870046ca26f5dab2828e
Birhane Kahsay <birhane.kahsay99@gmail.com>
2024-04-25 23:05:59 +0200

    Equilibrium point solver finction added

--- assignments.py
+++ assignments.py
@@ -18,5 +18,37 @@ def majority_element(number_list:list,size:int):
     else:
         return -1
         
-print(majority_element([3,1,3,3,1,1,2],7))
+print(majority_element([3,1,4,3,4,1,1,4,4,4,4,4],12))
+
+#Given an array A of n non negative numbers. 
+#The task is to find the first equilibrium point in an array. 
+#Equilibrium point in an array is an index (or position) such that the sum of all elements before that index is same as sum of elements after it.
+
+def equilibriumPoint(positive_integer_list:list, size:int) -> int:
+
+    if size == 1:
+        return 0
+    else:
+        half_index = size //2
+        while True:
+            l_summer = summer(positive_integer_list[0:half_index])
+            r_summer = summer(positive_integer_list[half_index+1:size])
+
+            if(l_summer > r_summer):
+                half_index -= 1
+            elif r_summer > l_summer:
+                half_index += 1
+            else:
+                return half_index
+
+def summer(integer_list:list) -> int:
+    sum = 0
+
+    for num in integer_list:
+        sum += num
+    return sum
+
+
+print(equilibriumPoint([8],1))
+
     
